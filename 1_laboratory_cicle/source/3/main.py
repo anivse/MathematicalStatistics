3ca81d88bf6015e4f17f26a11990b2cb2bcbe266
Anna Ivanova <anivse@users.noreply.github.com>
2023-03-24 16:02:45 +0300

    update source and report

--- 1_laboratory_cicle/source/3/main.py
+++ 1_laboratory_cicle/source/3/main.py
@@ -4,121 +4,203 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import math
 
-def share_of_emissions(x):
+def count_of_emissions(x):
     n = len(x)
-    p1 = 0.25
-    p2 = 0.75
-    Q1 = x[math.ceil(n*p1)-1]
-    Q3 = x[math.ceil(n*p2)-1]
-    X1 = Q3 - 1.5 * (Q3 - Q1)
+    Q1 = np.quantile(x, 0.25)
+    Q3 = np.quantile(x, 0.75)
+    X1 = Q1 - 1.5 * (Q3 - Q1)
     X2 = Q3 + 1.5 * (Q3 - Q1)
     count = 0 
     for i in range(0, n):
-        if ((x[i] < X1) or (x[i] > X2)):
+        if (x[i] < X1 or x[i] > X2):
             count += 1
-    return round((count / n), 2)
+    return count 
 
-
-def Normal():
+def normal_boxplot():
     size_1 = 20
     size_2 = 100
     x_1 = sps.norm(loc=0, scale=1).rvs(size=size_1)
     x_2 = sps.norm(loc=0, scale=1).rvs(size=size_2)
-    x_1.sort()
-    x_2.sort()
-    x_1_em = share_of_emissions(x_1)
-    x_2_em = share_of_emissions(x_2)
     plt.figure()
     plt.boxplot(x = [x_1, x_2], vert = False, labels = [20, 100])
     plt.title("Normal")
     plt.xlabel("x")
     plt.ylabel("n")
     plt.show()
-    print("Normal: ")
-    print(f'20: {x_1_em}')
-    print(f'100: {x_2_em}')
+
+def normal_share_of_emissions():
+    size_1 = 20
+    size_2 = 100
+    repeat = 1000
+    count_1 = 0
+    count_2 = 0
+    for i in range (0, repeat):
+        x_1 = sps.norm(loc=0, scale=1).rvs(size=size_1)
+        x_2 = sps.norm(loc=0, scale=1).rvs(size=size_2)
+        x_1.sort()
+        x_2.sort()
+        count_1 += count_of_emissions(x_1)
+        count_2 += count_of_emissions(x_2)
     
-def Cauchy():
+    x_1_share = count_1 / (size_1 * repeat)
+    x_2_share = count_2 / (size_2 * repeat)
+
+    print("Normal: ")
+    print(f'20: {x_1_share}')
+    print(f'100: {x_2_share}')
+    
+def cauchy_share_of_emissions():
+    size_1 = 20
+    size_2 = 100
+    repeat = 1000
+    count_1 = 0
+    count_2 = 0
+    for i in range (0, repeat):
+        x_1 = sps.cauchy().rvs(size=size_1)
+        x_2 = sps.cauchy().rvs(size=size_2)
+        x_1.sort()
+        x_2.sort()
+        count_1 += count_of_emissions(x_1)
+        count_2 += count_of_emissions(x_2)
+    
+    x_1_share = count_1 / (size_1 * repeat)
+    x_2_share = count_2 / (size_2 * repeat)
+
+    print("Cauchy: ")
+    print(f'20: {x_1_share}')
+    print(f'100: {x_2_share}')
+
+def cauchy_boxplot():
     size_1 = 20
     size_2 = 100
     x_1 = sps.cauchy().rvs(size=size_1)
     x_2 = sps.cauchy().rvs(size=size_2)
-    x_1.sort()
-    x_2.sort()
-    x_1_em = share_of_emissions(x_1)
-    x_2_em = share_of_emissions(x_2)
     plt.figure()
     plt.boxplot(x = [x_1, x_2], vert = False, labels = [20, 100])
     plt.title("Cauchy")
     plt.xlabel("x")
     plt.ylabel("n")
     plt.show()
-    print("Cauchy: ")
-    print(f'20: {x_1_em}')
-    print(f'100: {x_2_em}')
 
 
-def Laplace():
+def laplace_share_of_emissions():
+    size_1 = 20
+    size_2 = 100
+    repeat = 1000
+    count_1 = 0
+    count_2 = 0
+   
+    for i in range (0, repeat):
+        x_1 = sps.laplace(loc=0, scale=1/2**0.5).rvs(size=size_1)
+        x_2 = sps.laplace(loc=0, scale=1/2**0.5).rvs(size=size_2)
+        x_1.sort()
+        x_2.sort()
+        count_1 += count_of_emissions(x_1)
+        count_2 += count_of_emissions(x_2)
+    
+    x_1_share = count_1 / (size_1 * repeat)
+    x_2_share = count_2 / (size_2 * repeat)
+    
+    print("Laplace: ")
+    print(f'20: {x_1_share}')
+    print(f'100: {x_2_share}')
+
+
+def laplace_boxplot():
     size_1 = 20
     size_2 = 100
     x_1 = sps.laplace(loc=0, scale=1/2**0.5).rvs(size=size_1)
     x_2 = sps.laplace(loc=0, scale=1/2**0.5).rvs(size=size_2)
     x_1.sort()
     x_2.sort()
-    x_1_em = share_of_emissions(x_1)
-    x_2_em = share_of_emissions(x_2)
     plt.figure()
     plt.boxplot(x = [x_1, x_2], vert = False, labels = [20, 100])
     plt.title("Laplace")
     plt.xlabel("x")
     plt.ylabel("n")
     plt.show()
-    print("Laplace: ")
-    print(f'20: {x_1_em}')
-    print(f'100: {x_2_em}')
 
+def poisson_share_of_emissions():
+    size_1 = 20
+    size_2 = 100
+    repeat = 1000
+    count_1 = 0
+    count_2 = 0
+    
+    for i in range (0, repeat):
+        x_1 = sps.poisson(mu=10).rvs(size=size_1)
+        x_2 = sps.poisson(mu=10).rvs(size=size_2)
+        x_1.sort()
+        x_2.sort()
+        count_1 += count_of_emissions(x_1)
+        count_2 += count_of_emissions(x_2)
+    
+    x_1_share = count_1 / (size_1 * repeat)
+    x_2_share = count_2 / (size_2 * repeat)
 
-def Poisson():
+    print("Poisson: ")
+    print(f'20: {x_1_share}')
+    print(f'100: {x_2_share}')
+
+def poisson_boxplot():
     size_1 = 20
     size_2 = 100
     x_1 = sps.poisson(mu=10).rvs(size=size_1)
     x_2 = sps.poisson(mu=10).rvs(size=size_2)
     x_1.sort()
     x_2.sort()
-    x_1_em = share_of_emissions(x_1)
-    x_2_em = share_of_emissions(x_2)
     plt.figure()
     plt.boxplot(x = [x_1, x_2], vert = False, labels = [20, 100])
     plt.title("Poisson")
     plt.xlabel("x")
     plt.ylabel("n")
     plt.show()
-    print("Poisson: ")
-    print(f'20: {x_1_em}')
-    print(f'100: {x_2_em}')
 
+def uniform_share_of_emissions():
+    size_1 = 20
+    size_2 = 100
+    repeat = 1000
+    count_1 = 0
+    count_2 = 0
 
-def Uniform():
+    for i in range (0, repeat):
+        x_1 = sps.uniform(loc=-3**0.5, scale=2*3**0.5).rvs(size=size_1)
+        x_2 = sps.uniform(loc=-3**0.5, scale=2*3**0.5).rvs(size=size_2)
+        x_1.sort()
+        x_2.sort()
+        count_1 += count_of_emissions(x_1)
+        count_2 += count_of_emissions(x_2)
+    
+    x_1_share = count_1 / (size_1 * repeat)
+    x_2_share = count_2 / (size_2 * repeat)
+
+    print("Uniform: ")
+    print(f'20: {x_1_share}')
+    print(f'100: {x_2_share}')
+
+def uniform_boxplot():
     size_1 = 20
     size_2 = 100
     x_1 = sps.uniform(loc=-3**0.5, scale=2*3**0.5).rvs(size=size_1)
     x_2 = sps.uniform(loc=-3**0.5, scale=2*3**0.5).rvs(size=size_2)
     x_1.sort()
     x_2.sort()
-    x_1_em = share_of_emissions(x_1)
-    x_2_em = share_of_emissions(x_2)
     plt.figure()
     plt.boxplot(x = [x_1, x_2], vert = False, labels = [20, 100])
     plt.title("Uniform")
     plt.xlabel("x")
     plt.ylabel("n")
     plt.show()
-    print("Uniform: ")
-    print(f'20: {x_1_em}')
-    print(f'100: {x_2_em}')
 
-Normal()
-Cauchy()
-Laplace()
-Poisson()
-Uniform()
+# normal_boxplot()
+# cauchy_boxplot()
+# laplace_boxplot()
+# poisson_boxplot()
+# uniform_boxplot()
+
+
+normal_share_of_emissions()
+cauchy_share_of_emissions()
+laplace_share_of_emissions()
+poisson_share_of_emissions()
+uniform_share_of_emissions()
